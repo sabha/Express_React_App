c7bb5b2c662a23173cc6c32d891373d1a2a3a651
Sabhanayagam Baskaran <ctsuser1@MACLTUS54429>
2017-10-12 14:46:02 -0500

    Auth fix.

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 export const facebook = {
   clientID: '132061847445653',
   clientSecret: '0be67f686adae70f03dabc626d67bb2e',
-  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  callbackURL: 'http://beeing.info/auth/facebook/callback',
   profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
 };
 
 export const google = {
   clientID: '932492463303-qicsgmgndn4tp3asckpasb6e09ppct8a.apps.googleusercontent.com',
   clientSecret: 'Sr99owCfDtLSetwwTa1X1-zR',
-  callbackURL: 'http://localhost:3000/auth/google/callback',
+  callbackURL: 'http://beeing.info/auth/google/callback',
 };
