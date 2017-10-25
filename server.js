afb1c0d69b9d5f7b941db3c951c1d41931f110a0
sabhab1 <sabha.b@gmail.com>
2017-10-25 23:44:40 +0000

    config fields added.

--- server.js
+++ server.js
@@ -12,10 +12,14 @@ import { facebook, google } from './config';
 
 // Transform Facebook profile because Facebook and Google profile objects look different
 // and we want to transform them into user objects that have the same set of attributes
-const transformFacebookProfile = (profile) => ({
-  name: profile.name,
-  avatar: profile.picture.data.url,
-});
+const transformFacebookProfile = (profile) => {
+    return {
+        name: profile.name,
+        firstName: profile.first_name,
+        lastName: profile.last_name,
+        avatar: profile.picture.data.url,
+    }
+}
 
 // Transform Google profile into user object
 const transformGoogleProfile = (profile) => ({
@@ -25,16 +29,14 @@ const transformGoogleProfile = (profile) => ({
 
 // Register Facebook Passport strategy
 passport.use(new FacebookStrategy(facebook,
-  // Gets called when user authorizes access to their profile
-  async (accessToken, refreshToken, profile, done)
-    // Return done callback and pass transformed user object
-    => done(null, transformFacebookProfile(profile._json))
+  // Gets called when user authorizes access to their profile    // Return done callback and pass transformed user object
+  // Return done callback and pass transformed user object
+  async (accessToken, refreshToken, profile, done) => done(null, transformFacebookProfile(profile._json))
 ));
 
 // Register Google Passport strategy
 passport.use(new GoogleStrategy(google,
-  async (accessToken, refreshToken, profile, done)
-    => done(null, transformGoogleProfile(profile._json))
+  async (accessToken, refreshToken, profile, done) => done(null, transformGoogleProfile(profile._json))
 ));
 
 // Serialize user into the sessions
